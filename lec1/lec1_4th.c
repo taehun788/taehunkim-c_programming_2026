ec03fcc856a6ea43f1e9be0fdfc02ca94e348091
taehun788 <taehun788@yonsei.ac.kr>
2026-03-13 05:38:25 +0000

    C 프로그램: 기본적인 산술 연산을 수행하는 코드 추가

---
+++ lec1/lec1_4th.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a= 2;
+    float a2= 2.0;
+    int b=3;
+    float c = 3.0;
+    printf("a % b: %d\n", a % b);
+    printf("a %%% c: %f\n", a % c);
+    printf("a2 %%% b: %f\n", a2 % b);
+}
+//하나도모르겠다 인공지능아 도와줘~
